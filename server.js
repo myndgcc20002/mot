b5258fa6bee857e1ee51935677787d9afc077c7d
myndgcc20002 <100873277+myndgcc20002@users.noreply.github.com>
2022-11-16 22:26:27 +0700

    start

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hi")
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log("running request ...")
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h3>hello</h3>');
+    res.end();
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log("server is running on port 3000");
+})
