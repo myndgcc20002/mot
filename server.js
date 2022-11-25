a2e2a78383011c768b23f12052557ab11bf61e50
myndgcc20002 <100873277+myndgcc20002@users.noreply.github.com>
2022-11-26 06:16:54 +0700

    start

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log("running request ...")
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>hello</h3>');
-    res.end();
-})
+const hostname = '127.0.0.1';
+const port = 3000;
 
-server.listen(3000, 'localhost', () => {
-    console.log("server is running on port 3000");
-})
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
